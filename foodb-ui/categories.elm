6034c9f0020887f976937adeb2061f931bf15d07
Joshua Gevirtz <joshgev@gmail.com>
2018-09-10 14:09:15 +0200

    categories.elm can now add a category

--- foodb-ui/categories.elm
+++ foodb-ui/categories.elm
@@ -4,6 +4,7 @@ import Html.Events exposing (..)
 import Html.Attributes as Attributes
 import Http
 import Json.Decode as Decode
+import Json.Encode as Encode
 import Url.Builder as Url
 import Debug
 import Basics
@@ -17,7 +18,12 @@ init: () -> (Model, Cmd Msg)
 init _ = 
     ((Model []) "nah" "", getCategories) 
 
-type Msg = CreateCategory | GetCategories | Categories (Result Http.Error (List String))
+type Msg = 
+    CreateCategory 
+    | NewCategoryUpdate (String)
+    | GetCategories 
+    | Category (Result Http.Error String)
+    | Categories (Result Http.Error (List String))
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = 
@@ -27,7 +33,9 @@ update msg model =
             case result of 
                 Ok categories -> ({ model | categories = categories, message="yeah"}, Cmd.none)
                 Err e -> ({model | message = (Debug.log "a" (Debug.toString  e))}, Cmd.none)
-        CreateCategory -> ({model | message = "Created new category"}, Cmd.none)
+        CreateCategory -> ({model | message = model.newCategory }, postCategory model.newCategory)
+        Category _ -> (model, Cmd.none)
+        NewCategoryUpdate newCategory -> ({model | newCategory = newCategory}, Cmd.none)
 
 subscriptions: Model -> Sub Msg
 subscriptions model = Sub.none
@@ -38,7 +46,7 @@ view model =
         text model.message,
         ul [] (List.map toLi model.categories),
         div [] [
-            input [Attributes.placeholder "New Category", Attributes.value model.newCategory] [],
+            input [Attributes.placeholder "New Category", Attributes.value model.newCategory, onInput NewCategoryUpdate] [],
             button [onClick CreateCategory] [text "Add"]
         ]
     ]
@@ -48,5 +56,14 @@ toLi x = li [] [text x]
 getCategories : Cmd Msg
 getCategories = Http.send Categories (Http.get "http://localhost:8001/categories" categoriesDecoder)
 
+postCategory : String -> Cmd Msg
+postCategory name = Http.send  Category (Http.post "http://localhost:8001/categories" (Http.jsonBody (categoryEncoder name)) categoryDecoder)
+
+categoryEncoder: String -> Encode.Value
+categoryEncoder name = Encode.object [("name", Encode.string name)]
+
+categoryDecoder: Decode.Decoder String
+categoryDecoder = Decode.field "name" Decode.string 
+
 categoriesDecoder : Decode.Decoder (List String)
-categoriesDecoder = Decode.list (Decode.field "name" Decode.string)
+categoriesDecoder = Decode.list categoryDecoder
